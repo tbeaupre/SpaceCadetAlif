aa75d9fdce94bbea08270166242d1334c1223a74
Tyler Beaupre <tylerbeaupre4@gmail.com>
2017-12-19 14:57:25 -0500

    Added collision boxes to Body

--- SpaceCadetAlif/Source/Engine/Objects/Body.cs
+++ SpaceCadetAlif/Source/Engine/Objects/Body.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 
 namespace SpaceCadetAlif.Source.Engine.Objects
 {
+    /*
+     * The abstract parent class of both Actors and Props.
+     * Allows them to be used by the PhysicsManager in the same way.
+     */
+
     abstract class Body
     {
         public Sprite Sprite { get; }
+        public List<Rectangle> CollisionBoxes { get; }
         public Vector2 Position { get; set; }
         public Vector2 Velocity { get; set; }
         public Vector2 Acceleration { get; set; }
 
-        protected Body(Sprite sprite, Vector2 position)
+        protected Body(Sprite sprite, List<Rectangle> collisionBoxes, Vector2 position)
         {
             Sprite = sprite;
+            CollisionBoxes = collisionBoxes;
             Position = position;
             Velocity = Vector2.Zero;
             Acceleration = Vector2.Zero;
