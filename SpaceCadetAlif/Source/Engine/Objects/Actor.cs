baa71e1d53b2ef032286d1d1cb7fe5f92a717ef0
Tyler Beaupre <tylerbeaupre4@gmail.com>
2017-12-19 14:46:54 -0500

    Objects Class Hierarchy

---
+++ SpaceCadetAlif/Source/Engine/Objects/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace SpaceCadetAlif.Source.Engine.Objects
+{
+    class Actor : Body
+    {
+        public Actor(Sprite sprite, Vector2 position) : base(sprite, position)
+        {
+        }
+    }
+}
---
+++ SpaceCadetAlif/Source/Engine/Objects/Prop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace SpaceCadetAlif.Source.Engine.Objects
+{
+    class Prop : Body
+    {
+        public Prop(Sprite sprite, Vector2 position) : base(sprite, position)
+        {
+        }
+    }
+}
