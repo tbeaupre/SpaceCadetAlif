6518e9d448ca4a2ea68c8e59a818544607de41ff
Tyler Beaupre <tylerbeaupre4@gmail.com>
2017-12-26 21:25:05 -0500

    Objects are now responsible for adding/removing themselves from update lists

--- SpaceCadetAlif/Source/Engine/Objects/GameObject.cs
+++ SpaceCadetAlif/Source/Engine/Objects/GameObject.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using SpaceCadetAlif.Source.Engine.Events;
+using SpaceCadetAlif.Source.Engine.Managers;
 using SpaceCadetAlif.Source.Engine.Physics;
 using System.Collections.Generic;
 
@@ -17,8 +18,19 @@ namespace SpaceCadetAlif.Source.Engine.Objects
         public GameObject(List<Rectangle> collisionBoxes, Vector2 position)
         {
             Body = new Body(collisionBoxes, position);
+            OnCreate();
         }
 
         public virtual void Update() { }
+
+        public virtual void OnCreate()
+        {
+            WorldManager.ToUpdate.Add(this);
+        }
+
+        public virtual void OnDelete()
+        {
+            WorldManager.ToUpdate.Remove(this);
+        }
     }
 }
