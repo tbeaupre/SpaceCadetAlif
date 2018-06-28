a860af8540979725b9bd1d9277aa7121d0759157
Tyler Beaupre <tylerbeaupre4@gmail.com>
2018-06-28 14:02:02 -0400

    More pseudo code

--- SpaceCadetAlif/Source/Engine/Managers/PhysicsManager.cs
+++ SpaceCadetAlif/Source/Engine/Managers/PhysicsManager.cs
@@ -17,8 +17,12 @@ namespace SpaceCadetAlif.Source.Engine.Managers
 
     static class PhysicsManager
     {
+        private static Dictionary<GameObject, Vector2> impactResultants = new Dictionary<GameObject, Vector2>();
+
         public static void Update()
         {
+            impactResultants.Clear();
+
             // Update positions based on velocity
             UpdatePositions();
 
@@ -35,28 +39,64 @@ namespace SpaceCadetAlif.Source.Engine.Managers
         // Update object positions based on their current velocity.
         private static void UpdatePositions()
         {
-
+            foreach (GameObject obj in WorldManager.ToUpdate)
+            {
+                obj.Body.Position += obj.Body.Velocity;
+            }
         }
 
         // Correct objects that are clipping into one another.
         private static void ClipCorrection()
         {
-            foreach (GameObject obj in listofgameobjects)
+            foreach (GameObject obj in WorldManager.ToUpdate)
             {
-                foreach (GameObject otherobj in listofgameobjects)
+                foreach (GameObject otherobj in WorldManager.ToUpdate)
                 {
                     if (obj != otherobj)
                     {
-                        if (clipping)
+                        if (ClipCheck(obj, otherobj))
                         {
-                            change velocities;
-                            clip correct;
+                            ChangeImpactVelocity(obj, otherobj);
+                            CorrectClipping(obj, otherobj);
                         }
                     }
                 }
 
-                change velocity based on forces;
+                UpdateVelocity(obj);
             }
+        }
+
+        private static bool ClipCheck(GameObject obj1, GameObject obj2)
+        {
+            foreach (Rectangle collisionBox1 in obj1.Body.CollisionBoxes)
+            {
+                foreach (Rectangle collisionBox2 in obj2.Body.CollisionBoxes)
+                {
+                    if (collisionBox1.Intersects(collisionBox2))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        // Adds impact velocities to the impactResultants dictionary.
+        private static void ChangeImpactVelocity(GameObject obj1, GameObject obj2)
+        {
+
+        }
+
+        // Corrects clipping issues by snapping to edge.
+        private static void CorrectClipping(GameObject obj1, GameObject obj2)
+        {
+
+        }
+
+        // Update velocities based on impact resultants and gravity.
+        private static void UpdateVelocity(GameObject obj1)
+        {
+
         }
     }
 }
