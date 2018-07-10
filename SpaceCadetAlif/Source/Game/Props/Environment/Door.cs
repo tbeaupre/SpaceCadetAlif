ecf53af27c8bd7810eef0e730bc76b2972507da8
Tyler Beaupre <tylerbeaupre4@gmail.com>
2018-07-10 13:05:44 -0400

    Added doors

---
+++ SpaceCadetAlif/Source/Game/Props/Environment/Door.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using SpaceCadetAlif.Source.Engine;
+using SpaceCadetAlif.Source.Engine.Objects;
+using SpaceCadetAlif.Source.Engine.Graphics.Sprites;
+using SpaceCadetAlif.Source.Engine.Managers;
+
+namespace SpaceCadetAlif.Source.Game.Props.Environment
+{
+    /// <summary>
+    /// The physical manifestation of a standard door.
+    /// Includes a sprite and a portal which moves the player to another area.
+    /// </summary>
+    class Door : Prop
+    {
+        private static readonly List<Rectangle> DOOR_COLLISION_BOXES = new List<Rectangle> { new Rectangle(37, 0, 23, 30) };
+
+        private Portal mPortal; // The portal associated with this door.
+
+        public Door(Vector2 position, Room destRoom, Vector2 destPos)
+            : base (
+                  new List<Sprite> {
+                    new ManualSprite(ResourceManager.LoadSpriteData("Prop/Environment/Door", 1)) },
+                  new List<Rectangle> { },
+                  position,
+                  false,
+                  false,
+                  false)
+        {
+            mPortal = new Portal(destRoom, destPos, DOOR_COLLISION_BOXES, position);
+        }
+    }
+}
