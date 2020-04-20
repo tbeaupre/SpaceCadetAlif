ff8cb3a9aeab9411e7f89d803eedc0e7053e8c40
Tyler Beaupre <tbeaupre@wpi.edu>
2020-04-20 08:56:09 -0400

    Starting work on random map generation

---
+++ SpaceCadetAlif/Source/Engine/Map/Room.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+public class Room
+{
+    public int Up { get; set; }
+    public int Right { get; set; }
+    public int Down { get; set; }
+    public int Left { get; set; }
+}
---
+++ SpaceCadetAlif/Source/Engine/Map/RoomGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System;
+
+public class RoomGenerator
+{
+    int maxLength;
+    int maxTurns;
+    Room[,] rooms;
+
+    public RoomGenerator(int dimension, int maxLength, int maxTurns)
+    {
+        this.maxLength = maxLength;
+        this.maxTurns = maxTurns;
+        rooms = new Room[dimension, dimension];
+
+        // Pick a random starting point.
+        Random rand = new Random();
+        int currentColumn = rand.Next(dimension);
+        int currentRow = rand.Next(dimension);
+    }
+}
